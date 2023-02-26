03d5604f9682832e1714ce7b8aed4a05a518c026
juyeong-chungbuk <happyu9986@gmail.com>
2023-02-27 00:33:21 +0900

    add : 시계 메인 페이스 컴포넌트 구현

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ClockFace from "./components/ClockFace";
+import "./styles/color.css";
+import "./styles/reset.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ClockFace />
+    </div>
+  );
+}
+
+export default App;
