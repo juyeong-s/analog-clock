3b11d234f28a593693fb68eef3b0454c6606a1ab
juyeong-chungbuk <happyu9986@gmail.com>
2023-02-27 23:40:27 +0900

    chore : 컴포넌트 네이밍 변경

---
+++ src/components/CenterCircle/index.jsx
@@ -0,0 +1,6 @@
+import "./index.css";
+
+function CenterCircle() {
+  return <div className="center-circle"></div>;
+}
+export default CenterCircle;
--- src/components/ClockCenterCircle/index.jsx
+++
@@ -1,6 +0,0 @@
-import "./index.css";
-
-function ClockCenterCircle(props) {
-  return <div className="clock-center-circle"></div>;
-}
-export default ClockCenterCircle;
