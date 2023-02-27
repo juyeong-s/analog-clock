561d2b53d3ec04aaf2d709a7ee031ecdee9a5980
juyeong-chungbuk <happyu9986@gmail.com>
2023-02-27 23:41:31 +0900

    add : UI 로직 분리

---
+++ src/components/Dial/index.view.jsx
@@ -0,0 +1,34 @@
+import CenterCircle from "../CenterCircle";
+import Hand from "../Hand";
+import Index from "../Index";
+import "./index.css";
+
+function DialView({
+  hDeg,
+  mDeg,
+  sDeg,
+  index,
+  handleMouseEnterOnClock,
+  handleMouseMoveOnClock,
+  handleMouseLeaveOnClock,
+}) {
+  return (
+    <div
+      className="dial"
+      onMouseEnter={handleMouseEnterOnClock}
+      onMouseMove={handleMouseMoveOnClock}
+      onMouseLeave={handleMouseLeaveOnClock}
+    >
+      {index.map((num, idx) => (
+        <Index key={idx} num={num} />
+      ))}
+
+      <CenterCircle />
+      <Hand type="hour" deg={hDeg} />
+      <Hand type="minute" deg={mDeg} />
+      <Hand type="second" deg={sDeg} />
+    </div>
+  );
+}
+
+export default DialView;
